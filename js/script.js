aa19abd2fa3cc797380d6910704ebe411c2d787c
Rafael Adel <rafael.adel20@gmail.com>
2012-08-24 18:29:04 +0200

    added  comment

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ $(function(){
             $(this).stop().animate({top:0}, 200);            
         },
         function(){
+            //if textbox is active, stop sliding it back up
             if(document.activeElement.id !== 'mainTask'){
                 $(this).stop().animate({top:-80}, 200);        
             }
