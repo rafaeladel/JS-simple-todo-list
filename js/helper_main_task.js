c8ed1e2409dc619e0639e1679643e32210590699
Rafael Adel <rafael.adel20@gmail.com>
2012-08-31 19:12:46 +0200

    added object to store each task info, and refactored total info properties into one object

--- js/helper_main_task.js
+++ js/helper_main_task.js
@@ -1,9 +1,24 @@
 var MainUtil = {
-	tasksArr : [],
-	catArr : ["all"],
-	priorityArr: {'urgent':0, 'normal':0, 'low':0},
-	add : function(){		
-		
+	singleTaskInfo : [],	
+	totalTasksInfo : {
+		tasksNames : [],
+		tasksPriority : {
+			"urgent" : 0,
+			"normal" : 0,
+			"low" : 0
+		},
+		tasksCat : ["all"]
+	},
+	priorityArr: {'urgent':0, 'normal':0, 'low':0},	
+	setTaskInfo : function(name, category, priority, date){			
+		this.singleTaskInfo[name] = {
+			label : name,
+			category: category,
+			priority: priority,
+			date: "today"
+		}				
+	},
+	add : function(){	
 		var MTLabel = $("#mainTaskInput").val(),  //task label	
 			MTCategory = $("#mainCatInput").val().toLowerCase(), //task category 
 			MTPriority = $("#prioritySlider").slider("value"),  //task priority		
@@ -16,6 +31,7 @@ var MainUtil = {
 									<div class="checkButton"></div>\
 									<div class="optTrigger"></div>\
 									<div class="addSubButton"></div>\
+									<div class="dateInfo">Added at : 26/6/1990</div>\
 									<div class="mainOptions">\
 										<ul>\
 											<li id="mainInfo">Details</li>\
@@ -27,7 +43,7 @@ var MainUtil = {
 							</div>');
 							
 							
-		this.tasksArr.push(MTLabel);
+		this.totalTasksInfo.tasksNames.push(MTLabel);
 		
 		//setting label
 		MTContents.find(".mainTaskLabel").text(MTLabel);		
@@ -37,21 +53,21 @@ var MainUtil = {
 			MTCategory = "uncategorized";
 		}
 		MTContents.attr('data-cat', MTCategory);
-		if(this.catArr.indexOf(MTCategory) == -1){
-			this.catArr.push(MTCategory);
+		if(this.totalTasksInfo.tasksCat.indexOf(MTCategory) == -1){
+			this.totalTasksInfo.tasksCat.push(MTCategory);
 			$("#categories ul").append("<li>" + MTCategory +"</li>");
 		}
-		$("#mainCatInput").autocomplete("option", "source",this.catArr);
+		$("#mainCatInput").autocomplete("option", "source",this.totalTasksInfo.tasksCat);
 		
 		//setting priority marker color
 		if(MTPriority == 2){ 
-			this.priorityArr["urgent"]++;			
+			this.totalTasksInfo.tasksPriority["urgent"]++;			
 			MTContents.attr('data-priority', 'urgent').find(".mainMarker").css("background-color", "red");
 		} else if(MTPriority == 1){
-			this.priorityArr["normal"]++;
+			this.totalTasksInfo.tasksPriority["normal"]++;
 			MTContents.attr('data-priority', 'normal').find(".mainMarker").css("background-color", "black");
 		} else if(MTPriority == 0){
-			this.priorityArr["low"]++;
+			this.totalTasksInfo.tasksPriority["low"]++;
 			MTContents.attr('data-priority', 'low').find(".mainMarker").css("background-color", "blue");
 		}		
 		
