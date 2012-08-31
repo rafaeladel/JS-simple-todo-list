30a2fcd2a98d0ac1ba4ea2d14735bb1bd765e4cb
Rafael Adel <rafael.adel20@gmail.com>
2012-08-31 06:52:46 +0200

    added calcTaskInfo() function

--- js/helper_misc.js
+++ js/helper_misc.js
@@ -7,5 +7,25 @@ var Util = {
 	},
 	hideError : function(){
 		$("#errorMSG").fadeOut(100);
-	}
+	},
+	calcTaskInfo : function(update){
+		var urgentCount = 0 , normalCount = 0, lowCount = 0;
+		if(update == true){
+			$("#tasksWrapper .wholeTask").each(function(){
+				var currentTask = $(this);
+				if(currentTask.attr("data-priority") == "urgent"){
+					urgentCount++;
+				}else if(currentTask.attr("data-priority") == "normal"){
+					normalCount++;
+				}else if(currentTask.attr("data-priority") == "low"){
+					lowCount++;
+				}
+			});
+			MainUtil.priorityArr["urgent"] = urgentCount;
+			MainUtil.priorityArr["normal"] = normalCount;
+			MainUtil.priorityArr["low"] = lowCount;
+		}
+		$("#mainTaskInfo").text("All:"+ $("#tasksWrapper .wholeTask").length + " [ Urgent:" + MainUtil.priorityArr["urgent"] + " | Normal:"+ MainUtil.priorityArr["normal"] + " | Low:" + MainUtil.priorityArr["low"] + " ]");
+		$("#completedTaskInfo").text("All:" + $("#completedTasks .wholeTask").length);
+	}	
 }
