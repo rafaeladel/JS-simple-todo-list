19873fea44dd1d2fcb75ffea14d3a4192c9e662a
Rafael Adel <rafael.adel20@gmail.com>
2012-09-02 01:31:45 +0200

    modified showError() code, added time calculation method

--- js/helper_misc.js
+++ js/helper_misc.js
@@ -1,12 +1,9 @@
-var Util = {					
-	showError : function(msg){		
-		$("#errorMSG").slideDown(200).find("p").text(msg);
+var Util = {						
+	showError : function(msg){
 		$("#errorMSG #closeMSG").click(function(){
-			$("#errorMSG").fadeOut(100);
+			$("#errorMSG").stop().fadeOut(100);			
 		});
-	},
-	hideError : function(){
-		$("#errorMSG").fadeOut(100);
+		$("#errorMSG").find("p").text(msg).end().slideDown(200).delay(3000).slideUp(100);		
 	},
 	calcTaskInfo : function(update){
 		var urgentCount = 0 , normalCount = 0, lowCount = 0;
@@ -27,5 +24,28 @@ var Util = {
 		}
 		$("#mainTaskInfo").text("All:"+ $("#tasksWrapper .wholeTask").length + " [ Urgent:" + MainUtil.totalTasksInfo.tasksPriority["urgent"] + " | Normal:"+ MainUtil.totalTasksInfo.tasksPriority["normal"] + " | Low:" + MainUtil.totalTasksInfo.tasksPriority["low"] + " ]");
 		$("#completedTaskInfo").text("All:" + $("#completedTasks .wholeTask").length);
-	}	
+	},
+	getCurrentTime : function(format){
+		var now = new Date(),			
+			monthArr = ['Jan','Feb','Mar','Apr','May', 'June','July','Aug','Sep', 'Oct','Nov','Dec'],
+			currentM = monthArr[now.getMonth()],
+			currentD = now.getDate() < 10 ? "0" + now.getDate() : now.getDate(),
+			currentY = now.getFullYear();
+			
+		var hour = now.getHours(),
+			meridiem = hour < 12 ? "am" : "pm",
+			processedHour = hour > 12 ? hour - 12 : hour,
+			currentHr = processedHour == 0 ? 12 : processedHour;
+
+		var minute = now.getMinutes(),
+			currentMnt = minute < 10 ? "0" + minute : minute;
+
+		var currentTime = currentM + " " + currentD + ", " + currentY + " at " + currentHr + ":" + currentMnt + meridiem;
+		
+		if(format == true){
+			return currentTime;	
+		} else {
+			return now.getTime();
+		}
+	}
 }
