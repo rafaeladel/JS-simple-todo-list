c97eff302888fff171b0ad83d0fb0326434e6674
Rafael Adel <rafael.adel20@gmail.com>
2012-09-02 06:08:55 +0200

    separated delegated events of cancle and add buttons in subTaskAdd

--- js/script.js
+++ js/script.js
@@ -37,37 +37,34 @@ $(function(){
 		});
 	});
 
-	$("#addMain, #mainCatInput").on('click keypress', function(evt){	
-		if(evt.type == "click" || evt.type =="keypress"){
-			if((evt.type =="click" && evt.target.id == "addMain") ||
-				(evt.which == 13 && evt.target.id=="mainCatInput")){	
-					MainUtil.add();					
-					$(this).parent().fadeOut(100, function(){
-						$(this).siblings().fadeIn(100).find("input").val("").focus();
+	$("#addMain, #mainCatInput").on('click keypress', function(evt){		
+		if((evt.type =="click" && evt.target.id == "addMain") || (evt.which == 13 && evt.target.id=="mainCatInput")){	
+			MainUtil.add();					
+			$(this).parent().fadeOut(100, function(){
+				$(this).siblings().fadeIn(100).find("input").val("").focus();
+			});
+			
+			//Setting Tags functionality 
+			$("#categories li").each(function(){
+				var currentTag = $(this);						
+				$(this).on('click' , function(){
+					var oldTag = $("#categories li.selected");	
+					$(".wholeTask").each(function(){
+						if(currentTag.is(":first-child")){
+							$(this).show();
+						}else if ($(this).attr("data-cat") != currentTag.text()){
+							$(this).hide();
+						} else {
+							$(this).show();
+						}
 					});
-					
-					//Setting Tags functionality 
-					$("#categories li").each(function(){
-						var currentTag = $(this);						
-						$(this).on('click' , function(){
-							var oldTag = $("#categories li.selected");	
-							$(".wholeTask").each(function(){
-								if(currentTag.is(":first-child")){
-									$(this).show();
-								}else if ($(this).attr("data-cat") != currentTag.text()){
-									$(this).hide();
-								} else {
-									$(this).show();
-								}
-							});
-							oldTag.removeClass("selected");
-							currentTag.addClass("selected");
-						});
-					});	
-					Util.calcTaskInfo(false);
-					
-			}
+					oldTag.removeClass("selected");
+					currentTag.addClass("selected");
+				});
+			});	
+			Util.calcTaskInfo(false);				
 		}
+		
 	});
 	
 	$("#taskPool").on("mouseover", ".wholeTask", function(){
@@ -92,35 +89,33 @@ $(function(){
 		
 	});
 	
-	$("#tasksWrapper").on('click' , '.subTaskAdd .cancelSub, .subTaskAdd .addSub', function(){
-		if($(this).hasClass('cancelSub')){
-			$(this).parent().slideUp(100);
-		} else if($(this).hasClass('addSub')){
-			if($(this).siblings(".subTaskInput").val() == ""){
-				$(this).siblings(".subTaskInput").stop().effect('highlight' ,{}, 1000);
-				return false;
-			}
-			var STLabel = $(this).siblings(".subTaskInput").val(),
-				MTLabel = $(this).parent().siblings(".mainTaskWrapper").find(".mainTaskLabel").text();
-			if($.inArray(STLabel, MainUtil.mainTaskInfo[MTLabel].subTasks) != -1){
-				$(this).parent().siblings(".subTaskWrapper").find(".subTask").each(function(){
-					if($(this).find(".subTaskLabel").text() == STLabel){
-						$(this).effect('highlight' , {} , 1000);
-					}
-				});
-				Util.showError("Sub Task already exists!")					
-				return false;				
-			}
-			
-			
-			SubUtil.add($(this));
-			if($(this).parent().siblings(".subTaskWrapper").children().length == 1){
-				$(this).parent().siblings(".subTaskWrapper").show();
-			}			
-			$(this).parent().fadeOut(100);
-			$(this).siblings(".subTaskInput").val("");
+	$("#tasksWrapper").on('click' , '.subTaskAdd .cancelSub', function(){
+		$(this).parent().slideUp(100);
+	});
+
+	$("#tasksWrapper").on("click keypress", ".addSub" , function(evt){		
+		if($(this).siblings(".subTaskInput").val() == ""){
+			$(this).siblings(".subTaskInput").stop().effect('highlight' ,{}, 1000);
+			return false;
 		}
-		
+		var STLabel = $(this).siblings(".subTaskInput").val(),
+			MTLabel = $(this).parent().siblings(".mainTaskWrapper").find(".mainTaskLabel").text();
+		if($.inArray(STLabel, MainUtil.mainTaskInfo[MTLabel].subTasks) != -1){
+			$(this).parent().siblings(".subTaskWrapper").find(".subTask").each(function(){
+				if($(this).find(".subTaskLabel").text() == STLabel){
+					$(this).effect('highlight' , {} , 1000);
+				}
+			});
+			Util.showError("Sub Task already exists!")					
+			return false;				
+		}
+		SubUtil.add($(this));
+		if($(this).parent().siblings(".subTaskWrapper").children().length == 1){
+			$(this).parent().siblings(".subTaskWrapper").show();
+		}			
+		$(this).parent().fadeOut(100);
+		$(this).siblings(".subTaskInput").val("");
+					
 	});
 	
 	
